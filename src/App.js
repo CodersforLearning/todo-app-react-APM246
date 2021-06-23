3858f590a97d3655f1434e30d3de689d3a0f93ae
APM246 <apm0366@gmail.com>
2021-06-23 22:33:49 +0800

    implemented deleting tasks and marking tasks as complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { render } from '@testing-library/react';
 
 class Task extends React.Component {
   constructor(props) {
@@ -24,25 +25,41 @@ class Task extends React.Component {
   }
 }
 
-function ListItem(props) {
-  return <li> {props.name} </li>
-}
-
-class TaskList extends React.Component {
+class ListItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {className: ''}
+    this.handleDeletion = this.handleDeletion.bind(this);
+  }
+
+  handleDeletion() {
+    this.props.handleDoubleClick(this.props.name);
   }
 
   render() {
     return (
-      <ul> 
+      <li className={this.state.className}> 
+        <span onDoubleClick={this.handleDeletion}>
+          {this.props.name} 
+        </span> 
+        <input type="checkbox" onClick={() => this.setState({className: 'task-complete'})} /> 
+      </li>
+    );
+  }
+}
+
+class TaskList extends React.Component {
+
+  render() {
+    return (
+      <ol> 
           {
             // need to come up with proper key system
             this.props.tasks.map((task, index) => 
-              <ListItem name={task} key={index} />
-          )}
-      </ul>
+              <ListItem name={task} key={index} handleDoubleClick={this.props.handleDoubleClick}/>
+            )
+          }
+      </ol>
     );
   }
 }
@@ -54,6 +71,7 @@ class App extends React.Component {
     this.state = {value: '', tasks: []};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
   }
 
   handleChange(e) {
@@ -62,9 +80,16 @@ class App extends React.Component {
 
   handleSubmit(e) {
     let updated_tasks = this.state.tasks.concat(this.state.value);
-    this.setState({tasks: updated_tasks});
+    this.setState({tasks: updated_tasks, value: ''});
     e.preventDefault();
-    console.log(this.state.tasks.toString());
+  }
+
+  handleDoubleClick(task) {
+    let index = this.state.tasks.indexOf(task);
+    let tasks_copy = this.state.tasks.slice();
+    tasks_copy.splice(index, 1);
+    this.setState({tasks: tasks_copy})
+    
   }
 
   render() {
@@ -72,14 +97,19 @@ class App extends React.Component {
       <div className="App row">
         <div className="col-6 left">
           <div className="task-adder">
+            <p> To add tasks to your todo list, enter them in the text box and press
+              submit. To mark tasks as complete, click the checkbox beside them. To delete
+              tasks, double click on the task. 
+            </p>
             <form onSubmit={this.handleSubmit}>
               <input type="text" value={this.state.value} onChange={this.handleChange} />
-              <input type="submit" value="submit" />
+              <input className="submit" type="submit" value="submit" />
             </form>
           </div>
         </div>
         <div className="col-6 right">
-          <TaskList tasks={this.state.tasks} />
+          <h1 className="todo-header"> Todo List </h1>
+          <TaskList tasks={this.state.tasks} handleDoubleClick={this.handleDoubleClick}/>
         </div>
       </div>
     );
