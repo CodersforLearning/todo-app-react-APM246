e6f2348da9447233212db6e57a743ece540cc02c
APM246 <apm0366@gmail.com>
2021-06-25 01:24:05 +0800

    added Firebase authentication using email/password

--- src/App.js
+++ src/App.js
@@ -3,6 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { render } from '@testing-library/react';
+import firebase from "firebase/app"
+import "firebase/analytics"
+import "firebase/auth"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqqHDbwML8Q-5trbu-he4bgSvAOH6USE4",
+  authDomain: "todo-app-f9f30.firebaseapp.com",
+  projectId: "todo-app-f9f30",
+  storageBucket: "todo-app-f9f30.appspot.com",
+  messagingSenderId: "769594505515",
+  appId: "1:769594505515:web:f29c98515ac036fe606d6e",
+  measurementId: "G-XV346X9JN4"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+let firebaseui = require('firebaseui');
+let ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ]
+});
+
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      document.getElementById('root').style.display = "block";
+      document.getElementById('login').style.display = 'none';
+      return true;
+    }
+  }
+}
+
+ui.start('#firebaseui-auth-container', uiConfig);
 
 class Task extends React.Component {
   constructor(props) {
@@ -105,7 +142,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App row">
+      <div id="main-app" className="App row">
         <div className="col-6 left d-flex align-items-center justify-content-center">
           <div className="row row-cols-1">
             <div className="description col">
